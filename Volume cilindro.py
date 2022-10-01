88ddf83e9853885a9e2857631ed63efe56ef8ed6
ilGalghi <114179709+ilGalghi@users.noreply.github.com>
2022-10-02 00:00:25 +0200

    Add files via upload

---
+++ Volume cilindro.py
@@ -0,0 +1,5 @@
+var1 = input("Inserisci raggio del cilindro ")
+var2 = input("Inserisci altezza del cilindro ")
+var3 = float(var1)
+var4 = float(var2)
+print("Volume del cilindro: ", 3.1415*(var3**2)*var4)
