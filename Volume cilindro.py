12c10dbb8fe8255dcad551b24b81985ae658d2f1
ilGalghi <114179709+ilGalghi@users.noreply.github.com>
2023-09-13 23:15:51 +0200

    Update Volume cilindro.py

--- Volume cilindro.py
+++ Volume cilindro.py
@@ -1,5 +1,3 @@
-var1 = input("Inserisci raggio del cilindro ")
-var2 = input("Inserisci altezza del cilindro ")
-var3 = float(var1)
-var4 = float(var2)
-print("Volume del cilindro: ", 3.1415*(var3**2)*var4)
+raggio = float(input("Inserisci raggio del cilindro: "))
+altezza = float(input("Inserisci altezza del cilindro: "))
+print("Volume del cilindro: ", 3.1415*(raggio**2)*altezza)
