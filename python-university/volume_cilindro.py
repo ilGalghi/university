d7b89365eb95a11ff32a618ae935f3d39179c680
ilGalghi <114179709+ilGalghi@users.noreply.github.com>
2023-09-14 00:08:51 +0200

    Update volume_cilindro.py

--- python-university/volume_cilindro.py
+++ python-university/volume_cilindro.py
@@ -1,1 +1,5 @@
-#daje
+from math import pi
+
+raggio = float(input("Inserisci raggio del cilindro: "))
+altezza = float(input("Inserisci altezza del cilindro: "))
+print("Volume del cilindro: ", pi*(raggio**2)*altezza)
