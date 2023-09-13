be5e31fc851511156dd495d835838ed46e5fcc3e
ilGalghi <114179709+ilGalghi@users.noreply.github.com>
2023-09-14 00:08:41 +0200

    Delete Volume cilindro.py

--- Volume cilindro.py
+++
@@ -1,5 +0,0 @@
-from math import pi
-
-raggio = float(input("Inserisci raggio del cilindro: "))
-altezza = float(input("Inserisci altezza del cilindro: "))
-print("Volume del cilindro: ", pi*(raggio**2)*altezza)
