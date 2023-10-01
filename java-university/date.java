fe539edb8ad1d47c7494d57a25e2feb613d5f2b8
Leonardo <114179709+ilGalghi@users.noreply.github.com>
2023-10-01 17:08:31 +0200

    Create date.java

---
+++ java-university/date.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Data {
+
+	public static void main(String[] args) {
+		Date d;
+		d = new Date();
+		System.out.println(d);				//stampa data
+		
+		System.out.println(d.getMonth());	//stampa mese - deprecato (attenzione: Gennaio = 0)
+		d.setMonth(11);				//imposta mese a 11 (dicembre)
+
+		System.out.println(d);				//stampa nuova data
+
+	}
+}
