8635dd9c5786dff2a14f9fae8855a583d3b4501d
Leonardo <114179709+ilGalghi@users.noreply.github.com>
2024-01-31 01:06:56 +0100

    Delete java-university/Lezioni/Lez2023_10_1_TipoInsiemi directory

--- java-university/Lezioni/Lez2023_10_1_TipoInsiemi/src/Main_Sara.java
+++
@@ -1,9 +0,0 @@
-import org.w3c.dom.Node;
-
-public class Main_Sara {
-	public static void main(String[] args) {
-		Node s; 
-		//oppure non lo importi all'inizio e dichiari 
-		org.w3c.dom.Node b;
-	}
-}
